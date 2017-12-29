a7cf266bf351f9ca3033b85b5e7307b2b18ca357
huangrui <huangrui>
2017-12-29 11:28:55 +0800

    新增数据库脚本

---
+++ demo-api/src/main/resources/dubbo-demo2.sql
@@ -0,0 +1,17 @@
+
+CREATE DATABASE `dubbo-demo2`;
+use `dubbo-demo2`
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'a');
